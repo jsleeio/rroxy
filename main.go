8002cae9b7412e20282b842a5f7c1d3067162924
John Slee <john.slee@siteminder.com>
2018-09-20 18:18:45 +1000

    gofmt

--- main.go
+++ main.go
@@ -1,42 +1,42 @@
 package main
 
 import (
-  "flag"
-  "net/http"
-  "net/url"
-  "github.com/vulcand/oxy/forward"
-  "github.com/vulcand/oxy/roundrobin"
-  "github.com/vulcand/oxy/buffer"
-  )
+	"flag"
+	"github.com/vulcand/oxy/buffer"
+	"github.com/vulcand/oxy/forward"
+	"github.com/vulcand/oxy/roundrobin"
+	"net/http"
+	"net/url"
+)
 
 func main() {
-  // Forwards incoming requests to whatever location URL points to, adds proper forwarding headers
-  listen := flag.String("listen", ":8000", "TCP port to listen on for requests")
-  retrypredicate := flag.String("retry-predicate", "IsNetworkError() && Attempts() < 2", "Retry predicate (see: go doc github.com/vulcand/oxy/buffer.Retry)")
-  flag.Parse()
+	// Forwards incoming requests to whatever location URL points to, adds proper forwarding headers
+	listen := flag.String("listen", ":8000", "TCP port to listen on for requests")
+	retrypredicate := flag.String("retry-predicate", "IsNetworkError() && Attempts() < 2", "Retry predicate (see: go doc github.com/vulcand/oxy/buffer.Retry)")
+	flag.Parse()
 
-  fwd, _ := forward.New()
-  lb, _ := roundrobin.New(fwd)
+	fwd, _ := forward.New()
+	lb, _ := roundrobin.New(fwd)
 
-  for _,backend := range flag.Args() {
-    url,err := url.Parse(backend)
-    if err != nil {
-      panic(err)
-    }
-    err = lb.UpsertServer(url)
-    if err != nil {
-      panic(err)
-    }
-  }
+	for _, backend := range flag.Args() {
+		url, err := url.Parse(backend)
+		if err != nil {
+			panic(err)
+		}
+		err = lb.UpsertServer(url)
+		if err != nil {
+			panic(err)
+		}
+	}
 
-  buffer, err := buffer.New(lb, buffer.Retry(*retrypredicate))
-  if err != nil {
-    panic(err)
-  }
+	buffer, err := buffer.New(lb, buffer.Retry(*retrypredicate))
+	if err != nil {
+		panic(err)
+	}
 
-  s := &http.Server{
-    Addr:    *listen,
-    Handler: buffer,
-  }
-  s.ListenAndServe()
+	s := &http.Server{
+		Addr:    *listen,
+		Handler: buffer,
+	}
+	s.ListenAndServe()
 }
